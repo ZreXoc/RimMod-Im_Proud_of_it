3df13b59dea972a59d61640ea04f059ed24e86be
ZreXoc <zeexoc@outlook.com>
2024-08-10 22:57:15 +0800

    fix: 工作者为机械体时报错

--- Source/Patch/PatchMakeRecipeProducts.cs
+++ Source/Patch/PatchMakeRecipeProducts.cs
@@ -27,41 +27,42 @@ public class PatchMakeRecipeProducts
     [HarmonyPostfix]
     static IEnumerable<Thing> Postfix(IEnumerable<Thing> __result, PassState __state)
     {
-        
         var worker = __state.worker;
         var recipeDef = __state.recipeDef;
-        Log.Message(recipeDef.label);
+        // Log.Message(worker);
+        // Log.Message(recipeDef.label);
+
         foreach (var thing in __result)
         {
-
-        Log.Message($"wa: {recipeDef.WorkAmountTotal(thing)}");
-            QualityCategory qc;
-            if (thing.TryGetQuality(out qc))
-            {
-                Messages.Message(qc.GetLabel(), thing, MessageTypeDefOf.NeutralEvent);
-            }
+            // Log.Message($"wa: {recipeDef.WorkAmountTotal(thing)}");
 
             var settings = LoadedModManager.GetMod<Mod>().GetSettings<ImpModSettings>();
-    
+
             var options = new ImpCore.Options
             {
-               QI_Offset = settings.QI_Offset,
-               Mood_Offset = settings.Mood_Offset,
-               Time_Offset = settings.Time_Offset,
+                QI_Offset = settings.QI_Offset,
+                Mood_Offset = settings.Mood_Offset,
+                Time_Offset = settings.Time_Offset,
             };
-            
+
+
             // Log.Message(options.QI_Offset);
             // Log.Message(options.Mood_Offset);
             // Log.Message(options.Time_Offset);
 
-            var level = worker.skills.GetSkill(recipeDef.workSkill).Level;
-            var thought = ImpCore.GetFixedThoughtDefOf(level, qc,
-                recipeDef.WorkAmountTotal(thing), options);
-            
-            if (thought != null)
+            QualityCategory qc;
+
+            if (worker.skills !=null && thing.TryGetQuality(out qc))
             {
-                worker?.needs?.mood?.thoughts?.memories?.TryGainMemory(thought);
-                Log.Message($"thought:: days: {thought.durationDays}; mood: {thought.stages}");
+                var level = worker.skills.GetSkill(recipeDef.workSkill).Level;
+                var thought = ImpCore.GetFixedThoughtDefOf(level, qc,
+                    recipeDef.WorkAmountTotal(thing), options);
+
+                if (thought != null)
+                {
+                    worker?.needs?.mood?.thoughts?.memories?.TryGainMemory(thought);
+                    // Log.Message($"thought:: days: {thought.durationDays}; mood: {thought.stages}");
+                }
             }
 
             yield return thing;
